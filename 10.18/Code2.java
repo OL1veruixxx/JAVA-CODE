d686f136623328c6a457fdaac80a69c1afa716e6
F1owerSugar <106242212+OL1veruixxx@users.noreply.github.com>
2022-10-21 17:08:25 +0800

    DAY1

---
+++ 10.18/Code2.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static int func1()
+    {
+        int total=0,temp=0;
+        for(int i=1;i<=99;i++)
+        {
+            if(i%2!=0)
+            {
+                temp = i;
+                total += temp;
+            }
+
+        }
+
+        return total;
+
+
+
+
+
+
+    }
+
+
+
+    public static int func2(int m)
+    {
+        int total = 1;
+        do
+        {
+            total =total*m;
+            m--;
+
+
+        }
+        while(m!=0);
+        return total;
+
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.println(func1());
+        System.out.println(func2(5));
+    }
+}
